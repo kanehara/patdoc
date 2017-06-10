01564f1c749022a3c3c835a7050c626e6930c0a3
yohei.kanehara <yohei.kanehara@vodori.com>
2017-06-09 23:16:42 -0500

    Skeleton for Vuexˆ

--- client/src/main.js
+++ client/src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import vuex from 'vuex'
 
 Vue.config.productionTip = false
+
+Vue.use(vuex)
 
 /* eslint-disable no-new */
 new Vue({
