626edecde3b89ed81ce356616ec5eab5695f237e
yohei.kanehara <yohei.kanehara@vodori.com>
2017-06-10 12:51:14 -0500

    Added routes to profile, appointments, medical record

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Profile from '@/components/Profile'
+import Appointments from '@/components/Appointments'
+import MedicalRecord from '@/components/MedicalRecord'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path: '/profile',
+      component: Profile
+    }, {
+      path: '/appointments',
+      component: Appointments
+    }, {
+      path: '/medicalRecord',
+      component: MedicalRecord
     }
   ]
 })
