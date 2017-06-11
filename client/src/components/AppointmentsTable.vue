afa0ed3a2da5198fc38a97fbf7b7ba9a520b1ee5
yohei.kanehara <yohei.kanehara@vodori.com>
2017-06-10 19:41:10 -0500

    Added checkboxes for cancelling appointments

--- client/src/components/AppointmentsTable.vue
+++ client/src/components/AppointmentsTable.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="appointments">
-    <table class="ui celled table">
+    <table class="ui celled table definition">
       <thead>
       <tr>
+        <th></th>
         <th>Date</th>
         <th>Time</th>
         <th>Doctor</th>
@@ -18,6 +19,9 @@
                 negative: appointment.status === 'cancelled',
                 warning: appointment.status === 'pending'
               }">
+        <td class="collapsing">
+          <input type="checkbox" v-if="appointment.status !== 'cancelled'"/>
+        </td>
         <td>{{ appointment.date | formatDate }}</td>
         <td>{{ appointment.date | formatTime }}</td>
         <td>{{ appointment.doctor }}</td>
@@ -26,6 +30,22 @@
         <td>{{ appointment.status }}</td>
       </tr>
       </tbody>
+      <tfoot class="full-width">
+        <tr>
+          <th></th>
+          <th colspan="6">
+            <div class="ui right floated small primary labeled icon button">
+              <i class="calendar outline icon"></i> New Appointment
+            </div>
+            <div class="ui small disabled button">
+              Cancel
+            </div>
+            <div class="ui small button" @click="">
+              Cancel All
+            </div>
+          </th>
+        </tr>
+      </tfoot>
     </table>
   </div>
 </template>
