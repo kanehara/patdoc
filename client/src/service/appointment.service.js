be81eb7a258eaadf1129a7aba18a0be5408e74d9
yohei.kanehara <yohei.kanehara@vodori.com>
2017-06-11 14:21:51 -0500

    Added service method for declining app's

--- client/src/service/appointment.service.js
+++ client/src/service/appointment.service.js
@@ -7,58 +7,77 @@ const appointments = {
       subject: 'Wart removal',
       notes: 'I need a wart removed from my thumb.',
       doctor: 'Dr. Hans Zimmer',
-      status: 'Confirmed'
+      status: 'Confirmed',
+      declinationReason: null
     }, {
       id: 2,
       date: new Date('October 14, 2017 11:13:00'),
       subject: 'Knee surgery',
       notes: 'Knee surgery to be done',
       doctor: 'Dr. Hans Zimmer',
-      status: 'Pending'
+      status: 'Pending',
+      declinationReason: null
     }, {
       id: 3,
       date: new Date('October 13, 2017 01:13:00'),
       subject: 'Complex heart procedure',
       notes: 'Some complex procdure',
       doctor: 'Dr. Hans Zimmer',
-      status: 'Pending'
+      status: 'Pending',
+      declinationReason: null
     }, {
       id: 4,
       date: new Date('January 13, 2017 11:13:00'),
       subject: 'Wart removal',
       notes: 'I need a wart removed from my thumb.',
       doctor: 'Dr. Hans Zimmer',
-      status: 'Confirmed'
+      status: 'Confirmed',
+      declinationReason: null
     }, {
       id: 5,
       date: new Date('March 13, 2017 11:13:00'),
       subject: 'Wart removal',
       notes: 'I need a wart removed from my thumb.',
       doctor: 'Dr. Hans Zimmer',
-      status: 'Confirmed'
+      status: 'Confirmed',
+      declinationReason: null
     }, {
       id: 6,
       date: new Date('May 13, 2017 11:13:00'),
       subject: 'Wart removal',
       notes: 'I need a wart removed from my thumb.',
       doctor: 'Dr. Hans Zimmer',
-      status: 'Pending'
+      status: 'Pending',
+      declinationReason: null
     }
   ],
   2: [],
   3: []
 }
 
+async function getAppointment ({ patientId, appointmentId }) {
+  const idx = appointments[patientId].findIndex(a => a.id === appointmentId)
+  if (idx !== -1) {
+    return Promise.resolve(appointments[patientId])
+  }
+}
+
 export default {
-  getAppointment (userId) {
-    return appointments[userId] ? Promise.resolve(appointments[userId].slice())
-      : Promise.reject(`Cannot find appointments for user with id: ${userId}`)
+  async getAppointments ({ patientId }) {
+    return appointments[patientId] ? Promise.resolve(appointments[patientId].slice())
+      : Promise.reject(`Cannot find appointments for user with id: ${patientId}`)
   },
 
-  cancelAppointment (userId, appointmentId) {
-    const idx = appointments[userId].findIndex(a => a.id === appointmentId)
-    if (idx !== -1) {
-      appointments[userId].splice(idx, 1)
-    }
+  getAppointment,
+
+  async cancelAppointment ({ patientId, appointmentId }) {
+    const appointment = await getAppointment({ patientId, appointmentId })
+    appointment.status = 'Cancelled'
+  },
+
+  async declineAppointment ({ patientId, appointmentId, declinationReason }) {
+    const appointment = await getAppointment({ patientId, appointmentId })
+    appointment.status = 'Declined'
+    appointment.declinationReason = declinationReason
   }
 }
