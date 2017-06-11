ed9f9670eb243b978362056ed9afc198353818a2
yohei.kanehara <yohei.kanehara@vodori.com>
2017-06-11 02:19:31 -0500

    Cap'd status

--- client/src/service/appointment.service.js
+++ client/src/service/appointment.service.js
@@ -7,42 +7,42 @@ const appointments = {
       subject: 'Wart removal',
       notes: 'I need a wart removed from my thumb.',
       doctor: 'Dr. Hans Zimmer',
-      status: 'confirmed'
+      status: 'Confirmed'
     }, {
       id: 2,
       date: new Date('October 14, 2017 11:13:00'),
-      subject: 'Wart removal',
-      notes: 'I need a wart removed from my thumb.',
+      subject: 'Knee surgery',
+      notes: 'Knee surgery to be done',
       doctor: 'Dr. Hans Zimmer',
-      status: 'cancelled'
+      status: 'Pending'
     }, {
       id: 3,
       date: new Date('October 13, 2017 01:13:00'),
-      subject: 'Wart removal',
-      notes: 'I need a wart removed from my thumb.',
+      subject: 'Complex heart procedure',
+      notes: 'Some complex procdure',
       doctor: 'Dr. Hans Zimmer',
-      status: 'pending'
+      status: 'Pending'
     }, {
       id: 4,
       date: new Date('January 13, 2017 11:13:00'),
       subject: 'Wart removal',
       notes: 'I need a wart removed from my thumb.',
       doctor: 'Dr. Hans Zimmer',
-      status: 'confirmed'
+      status: 'Confirmed'
     }, {
       id: 5,
       date: new Date('March 13, 2017 11:13:00'),
       subject: 'Wart removal',
       notes: 'I need a wart removed from my thumb.',
       doctor: 'Dr. Hans Zimmer',
-      status: 'confirmed'
+      status: 'Confirmed'
     }, {
       id: 6,
       date: new Date('May 13, 2017 11:13:00'),
       subject: 'Wart removal',
       notes: 'I need a wart removed from my thumb.',
       doctor: 'Dr. Hans Zimmer',
-      status: 'cancelled'
+      status: 'Pending'
     }
   ],
   2: [],
