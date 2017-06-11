a160c7e16be0bc20df8e417a98d9e7f1d484f06c
yohei.kanehara <yohei.kanehara@vodori.com>
2017-06-11 15:44:14 -0500

    Server entry point

---
+++ server/index.js
@@ -0,0 +1,19 @@
+/**
+ * This file is only executed in dev mode
+ * In prod, we will pre-compile src/ with Babel and build it to lib/ and execute that instead
+ *
+ * See package.json for details of the scripts
+ */
+
+const assert = require('assert')
+
+assert(process.env.NODE_ENV !== 'production')
+
+// Read in environment variables from .env
+require('dotenv').config()
+
+// Babel hook
+require('babel-core/register')
+
+// Server
+require('./src')
