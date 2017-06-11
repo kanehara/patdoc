de9020d7194121927d7bb91744507b578549f3c0
yohei.kanehara <yohei.kanehara@vodori.com>
2017-06-11 18:48:38 -0500

    Updated server to allow CORS

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,15 @@ import { initAppointmentsRoutes } from './routes'
 const app = express()
 
 /**
+ * Allow CORS
+ */
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
+/**
  * Health check endpoint
  */
 app.get('/healthz', (req, res) => {
