dd7477d0ae52d168e11f7bd3d3f9f4b3b9604665
yohei.kanehara <yohei.kanehara@vodori.com>
2017-06-10 21:27:55 -0500

    Created Home component

---
+++ client/src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Welcome to PatDoc!</h1>
+    <router-link to="/patients/1">Patient 1</router-link>
+    <router-link to="/patients/2">Patient 2</router-link>
+    <router-link to="/patients/3">Patient 3</router-link>
+  </div>
+</template>
+
+<script>
+</script>
