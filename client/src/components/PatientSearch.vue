02f43f718ce18d5c34fa5ef995978804c393f4c1
yohei.kanehara <yohei.kanehara@vodori.com>
2017-06-14 01:30:16 -0500

    Added links to patient search rows

--- client/src/components/PatientSearch.vue
+++ client/src/components/PatientSearch.vue
@@ -2,18 +2,7 @@
   <div>
     <h1>Patient Search</h1>
     <div class="ui divided items">
-      <router-link to="/patients" v-for="patient in patients" class="item" :key="patient._id">
-        <div class="ui tiny image">
-          <img src="../assets/placeholder.png"/>
-        </div>
-        <div class="top aligned content">
-          <div class="header">{{ patient.name }}</div>
-          <div class="meta">
-            <div>{{ patient.phoneNumber | formatPhoneNumber }}</div><br/>
-            <div>{{ patient.emailAddress }}</div>
-          </div>
-        </div>
-      </router-link>
+      <PatientSearchRow v-for="patient in patients" :key="patient._id" :patient="patient"></PatientSearchRow>
     </div>
   </div>
 </template>
@@ -21,37 +10,25 @@
 <script>
   import * as actionTypes from '../store/modules/patients/action-types'
   import { mapGetters } from 'vuex'
+  import PatientSearchRow from './PatientSearchRow.vue'
 
   export default {
+    components: {
+      PatientSearchRow
+    },
     created () {
       this.$store.dispatch(actionTypes.GET_PATIENTS)
     },
     computed: {
       ...mapGetters(['patients'])
-    },
-    filters: {
-      formatPhoneNumber (phoneNumberInt) {
-        const phoneNumber = String(phoneNumberInt)
-        return `(${phoneNumber.substr(0, 3)}) ${phoneNumber.substr(3, 3)}-${phoneNumber.substr(6)}`
-      }
     }
   }
 </script>
 
 <style lang="less" scoped>
-  @import '../style/colors.less';
-
   .items {
     text-align: left;
     margin: 20px 20%;
-  }
-
-  .item {
-    &:hover {
-      background: @whiteGray
-    }
-
-    text-decoration: none;
   }
 
   .ui.divided.items>.item:first-child {
@@ -60,10 +37,5 @@
 
   .ui.divided.items>.item:last-child {
     padding-bottom: 1em !important;
-  }
-
-  .header {
-    position: static;
-    text-align: left;
   }
 </style>
---
+++ client/src/components/PatientSearchRow.vue
@@ -0,0 +1,48 @@
+<template>
+  <router-link :to="profileUrl" class="item">
+    <div class="ui tiny image">
+      <img src="../assets/placeholder.png"/>
+    </div>
+    <div class="top aligned content">
+      <div class="header">{{ patient.name }}</div>
+      <div class="meta">
+        <div>{{ patient.phoneNumber | formatPhoneNumber }}</div><br/>
+        <div>{{ patient.emailAddress }}</div>
+      </div>
+    </div>
+  </router-link>
+</template>
+
+<script>
+  export default {
+    props: ['patient'],
+    computed: {
+      profileUrl () {
+        return `/patients/${this.patient._id}`
+      }
+    },
+    filters: {
+      formatPhoneNumber (phoneNumberInt) {
+        const phoneNumber = String(phoneNumberInt)
+        return `(${phoneNumber.substr(0, 3)}) ${phoneNumber.substr(3, 3)}-${phoneNumber.substr(6)}`
+      }
+    }
+  }
+</script>
+
+<style scoped lang="less">
+  @import '../style/colors.less';
+
+  .item {
+    &:hover {
+      background: @whiteGray
+    }
+
+    text-decoration: none;
+  }
+
+  .header {
+    position: static;
+    text-align: left;
+  }
+</style>
