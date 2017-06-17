49f39cfee89ac793ffc49e2dc5aa5ccb94daf0ef
yohei.kanehara <yohei.kanehara@vodori.com>
2017-06-17 00:21:55 -0500

    First healthz check test

---
+++ server/test/integration/appointments.spec.js
@@ -0,0 +1,12 @@
+import chaiHttp from "chai-http";
+import chai from "chai";
+import prepareMockServer from '../prepare-mock-server';
+import logger from '../../src/logger'
+
+chai.use(chaiHttp);
+const request = chai.request;
+const expect = chai.expect;
+
+describe('Appointments', () => {
+
+})
