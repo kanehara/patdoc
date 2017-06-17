c325b7a2a72165912ae04c4b19e6b46d1105fd14
yohei.kanehara <yohei.kanehara@vodori.com>
2017-06-16 21:34:56 -0500

    Filter functionality for search

--- client/src/components/PatientSearch.vue
+++ client/src/components/PatientSearch.vue
@@ -4,12 +4,13 @@
     <div class="ui form">
       <div class="fields">
         <div class="field">
-          <input type="text" placeholder="Search"/>
+          <input type="text" placeholder="Search" v-model="searchInput" @keyup="searchPatients"/>
         </div>
       </div>
     </div>
     <div class="ui divided items">
-      <PatientSearchRow v-for="patient in getPatients" :key="patient._id" :patient="patient"></PatientSearchRow>
+      <PatientSearchRow v-for="patient in filteredPatients" :key="patient._id" :patient="patient"></PatientSearchRow>
+      <h3 v-if="!filteredPatients || !filteredPatients.length">No Results</h3>
     </div>
   </div>
 </template>
@@ -20,16 +21,40 @@
   import PatientSearchRow from './PatientSearchRow.vue'
   import store from '@/store'
   import config from '@/config'
+  import debounce from 'lodash.debounce'
 
   export default {
+    data () {
+      return {
+        searchInput: '',
+        searchVal: ''
+      }
+    },
     components: {
       PatientSearchRow
     },
     created () {
       this.$store.dispatch(actionTypes.GET_PATIENTS)
     },
+    methods: {
+      searchPatients () {
+        const updateSearchVal = debounce(vm => { vm.searchVal = vm.searchInput }, 200)
+        updateSearchVal(this)
+      }
+    },
     computed: {
-      ...mapGetters(['getPatients'])
+      ...mapGetters(['getPatients']),
+      filteredPatients () {
+        if (this.searchVal && this.searchVal.length) {
+          return this.getPatients.filter(p => {
+            const patientName = p.name.toLowerCase().trim()
+            const input = this.searchVal.toLowerCase().trim()
+            return patientName.indexOf(input) !== -1
+          })
+        } else {
+          return this.getPatients
+        }
+      }
     },
     beforeRouteEnter (to, from, next) {
       if (store.state.login.userType !== config.USER_TYPES.DOCTOR) {
