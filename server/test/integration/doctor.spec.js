1c19b796e9a9c3c2a25c62262e006f1f355416f7
yohei.kanehara <yohei.kanehara@vodori.com>
2017-06-17 23:24:50 -0500

    Doctor tests

---
+++ server/test/integration/doctor.spec.js
@@ -0,0 +1,28 @@
+import chaiHttp from 'chai-http'
+import chai from 'chai'
+import prepareMockServer from '../prepare-mock-server'
+import logger from '../../src/logger'
+
+chai.use(chaiHttp)
+chai.use(require('chai-datetime'))
+const request = chai.request
+const expect = chai.expect
+
+describe('Doctor', () => {
+  let server
+  before(async () => {
+    try {
+      server = await prepareMockServer()
+    } catch (err) {
+      logger.error(err)
+    }
+  })
+
+  it('Gets all doctors', async () => {
+    const response = await request(server)
+      .get('/doctors')
+    expect(response).to.have.status(200)
+    expect(response.body).to.have.lengthOf(3)
+    expect(response.body.find(d => d.emailAddress === 'doc@test.com')).to.be.ok
+  })
+})
