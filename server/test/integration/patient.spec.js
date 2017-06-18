fb8c11ca9347b3419d40ddd4f7fc32f703a0f80e
yohei.kanehara <yohei.kanehara@vodori.com>
2017-06-18 00:10:16 -0500

    Patient REST tests

---
+++ server/test/integration/patient.spec.js
@@ -0,0 +1,44 @@
+import chaiHttp from 'chai-http'
+import chai from 'chai'
+import prepareMockServer from '../prepare-mock-server'
+import logger from '../../src/logger'
+
+chai.use(chaiHttp)
+const request = chai.request
+const expect = chai.expect
+
+describe('Patients', () => {
+  let server
+  before(async () => {
+    try {
+      server = await prepareMockServer()
+    } catch (err) {
+      logger.error(err)
+    }
+  })
+
+  it('GET to retrieve all patients', async () => {
+    const res = await request(server)
+      .get('/patients')
+    expect(res).to.have.status(200)
+    expect(res.body).to.have.lengthOf(10)
+  })
+
+  it('GET to retrieve a single patient', async () => {
+    const allRes = await request(server)
+      .get('/patients')
+    const singleRes = await request(server)
+      .get(`/patients/${allRes.body[0]._id}`)
+    expect(singleRes).to.have.status(200)
+    expect(singleRes.body[0]).to.deep.equal(allRes.body[0])
+  })
+
+  it('returns 500 when trying to GET non existent user', async () => {
+    try {
+      await request(server)
+        .get('/patients/invalid')
+    } catch (err) {
+      expect(err.response).to.have.status(500)
+    }
+  })
+})
